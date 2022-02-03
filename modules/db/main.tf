65b9d3f5958da740531bef12b08fd74d62dcf816
Yagorus <yegor.sidenko@gmail.com>
2022-02-03 23:01:31 +0200

    add db

--- modules/db/main.tf
+++ modules/db/main.tf
@@ -14,7 +14,6 @@ resource "aws_db_instance" "db_instance" {
 resource "random_password" "password" {
   length           = 16
   special          = true
-  override_special = "_%@"
 }
 
 resource "aws_ssm_parameter" "token" {
