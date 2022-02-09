8223903b1aa7c8b6f7d91aa74fdcae37cb189d08
Yagorus <yegor.sidenko@gmail.com>
2022-02-09 16:41:37 +0200

    change

--- modules/cluster/role.tf
+++ modules/cluster/role.tf
@@ -7,6 +7,11 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_ecs_read_only" {
+  role = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
 resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = aws_iam_role.ecsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
